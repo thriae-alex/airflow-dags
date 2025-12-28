2324ca3281421be626d3a9d2149156ac518f351f
Alexandre Garcia <alexandregarcia@Alexandres-MacBook-Pro.local>
2025-12-28 21:27:56 +0100

    test file sync

---
+++ dags/test.py
@@ -0,0 +1,11 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="test_dag",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+    EmptyOperator(task_id="start")
